6d2f51394c47247d8aa3dace2f9eb08783c260cb
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2022-12-02 16:33:29 +0800

    1st commit - import starter files, add player that jumps using force when on ground, add obstacle which spawns at an interval, obstacle and background constantly scrolls to the left

---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obsataclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2.0f;
+    private float repeatRate = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obsataclePrefab, spawnPos, obsataclePrefab.transform.rotation);
+    }
+}
