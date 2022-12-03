e045229db33c6e9c16c8617080f90b2e27a325eb
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2022-12-03 15:52:37 +0800

    6th commit - randomize the spawn interval of obstacles

--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -8,7 +8,6 @@ public class SpawnManager : MonoBehaviour
 
     private Vector3 spawnPos = new Vector3(30, 0, 0);
     private float startDelay = 2.0f;
-    private float repeatRate = 2.0f;
     private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
@@ -18,7 +17,7 @@ public class SpawnManager : MonoBehaviour
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
 
         // Spawn obstacles on a delay and a rate
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        Invoke("SpawnObstacle", startDelay);
     }
 
     // Update is called once per frame
@@ -34,6 +33,10 @@ public class SpawnManager : MonoBehaviour
         {
             int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
             Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+
+            float randomDelay = Random.Range(1.5f, 3.0f);
+            Invoke("SpawnObstacle", randomDelay);
         }
+
     }
 }
