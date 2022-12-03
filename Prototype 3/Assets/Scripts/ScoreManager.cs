8b0444970daa859d45e7fffcd5a90d2f367ab65f
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2022-12-03 17:37:28 +0800

    8th commit - added a boost which increase speed of the game, added score system whichi s logged and increases a lot during boost

--- Prototype 3/Assets/Scripts/ScoreManager.cs
+++ Prototype 3/Assets/Scripts/ScoreManager.cs
@@ -2,40 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveLeft : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
+    private float totalTime;
+    private int multiplier = 10;
+    private float score;
+
     private PlayerController playerControllerScript;
-    private float leftBound = -15.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get variables from player controller scripts
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If the game is not yet over, move the object to the left
+
         if (!playerControllerScript.gameOver)
         {
-            float speed = 30.0f;
+             int boostMultiplier = 1;
 
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                speed = 60.0f;
+                boostMultiplier = 5;
             }
 
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
 
+            totalTime += Time.deltaTime * boostMultiplier;
+            score = totalTime * multiplier;
 
+            Debug.Log((int)score);
         }
 
-        // Destroy obstacle upon reaching the left bound
-        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
-        {
-            Destroy(gameObject);
-        }
     }
 }
